6b9848bd18280b2eea52e255225478ab5b162b00
Kjell Breiland <kjell.breiland@gmail.com>
2019-03-15 18:41:09 -0700

    make comment make sense

--- julian.go
+++ julian.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-// JulianDate takes a time.Time and turns it into a julien date - just formatting
+// JulianDate takes a time.Time and turns it into a julian date - just formatting
 func JulianDate(t time.Time) string {
 	return fmt.Sprintf("%04d%03d", t.Year(), t.YearDay())
 }
@@ -21,7 +21,7 @@ func JulianTimeNow() string {
 	return fmt.Sprintf("%v %s", JulianNow(), JulianTimePart(n))
 }
 
-// JulianTimePart takes
+// JulianTimePart returns the time part of a julian formatted time
 func JulianTimePart(t time.Time) string {
 	return fmt.Sprintf("%02d:%02d:%02d", t.Hour(), t.Minute(), t.Second())
 }
