8d2262e7b6d7b7b028225dc3465427c76a97c4a1
Kjell Breiland <kjell.breiland@gmail.com>
2019-03-15 18:42:07 -0700

    format as string and not %v

--- julian.go
+++ julian.go
@@ -18,7 +18,7 @@ func JulianNow() string {
 // JulianTimeNow returns the JulianDate with time for right now
 func JulianTimeNow() string {
 	n := time.Now()
-	return fmt.Sprintf("%v %s", JulianNow(), JulianTimePart(n))
+	return fmt.Sprintf("%s %s", JulianNow(), JulianTimePart(n))
 }
 
 // JulianTimePart returns the time part of a julian formatted time
