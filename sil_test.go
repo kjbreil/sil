db4736896b1f942513d7780d9900f7bcc2adc09e
Kjell Breiland <kjell.breiland@gmail.com>
2019-04-12 17:04:19 -0700

    update test, still bad tho

--- sil_test.go
+++ sil_test.go
@@ -1,7 +1,6 @@
 package sil
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/kjbreil/sil/loc"
@@ -25,42 +24,26 @@ func TestMake(t *testing.T) {
 		F01: "9902",
 	})
 
-	// m.AppendView("OBJ", loc.OBJ{
-	// 	F01: "0000000009902",
-	// })
-	// n := 1
-	// m.AppendView("OBJ", loc.OBJ{
-	// 	F01: "0000000009087",
-	// 	F17: &n,
-	// })
-	// m.AppendView("OBJ", loc.OBJ{
-	// 	F01: "0000000009902",
-	// })
+	m.AppendView("OBJ", loc.OBJ{
+		F01: "0000000009902",
+	})
+	n := 1
+	m.AppendView("OBJ", loc.OBJ{
+		F01: "0000000009087",
+		F17: &n,
+	})
+	m.AppendView("OBJ", loc.OBJ{
+		F01: "0000000009902",
+	})
 
-	// m["PRICE"] = Make("PRICE", loc.PRICE{})
-	// m["PRICE"].View.Data = append(m["PRICE"].View.Data, loc.PRICE{
-	// 	F01: "0000000009902",
-	// })
+	m["PRICE"] = Make("PRICE", loc.PRICE{})
+	m["PRICE"].View.Data = append(m["PRICE"].View.Data, loc.PRICE{
+		F01: "0000000009902",
+	})
 
-	str, err := m.Marshal()
+	_, err := m.Marshal()
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Log("Putting out")
-	fmt.Println(string(str))
 
-	// ss, err := s.Optional()
-	// if err != nil {
-	// 	t.Fatalf("failed to get optional: %v", err)
-	// }
-
-	// for _, es := range ss {
-	// 	b, err := es.Bytes()
-	// 	if err != nil {
-	// 		t.Fatalf("failed to convert to bytes with: %v", err)
-	// 	}
-	// 	t.Log(string(b))
-	// }
-
-	t.Fail()
 }
