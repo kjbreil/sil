f5eebc8515df5db88928d947877b6b326a4a8924
Kjell Breiland <kjell.breiland@gmail.com>
2019-05-04 14:20:47 -0700

    new test to use generated types

--- sil_test.go
+++ sil_test.go
@@ -2,36 +2,38 @@ package sil
 
 import (
 	"testing"
+
+	"github.com/kjbreil/sil/loc"
 )
 
 // OBJ will probably not work for an actual SIL file, this is for testing
 // creation from a type
-type OBJ struct {
-	UPC     string `sil:"F01"`
-	DEFAULT int    `sil:"F16" default:"6"`
-	POINTER *int   `sil:"F17"`
-}
+// type OBJ struct {
+// 	UPC     string `sil:"F01"`
+// 	DEFAULT int    `sil:"F16" default:"6"`
+// 	POINTER *int   `sil:"F17"`
+// }
 
 func TestMake(t *testing.T) {
 	m := make(Multi)
 
 	// m["OBJ"] = Make("OBJ", loc.OBJ{})
-	m.Make("OBJ", OBJ{})
+	m.Make("OBJ", loc.ObjTab{})
 
-	m.AppendView("OBJ", OBJ{
-		UPC: "9902",
+	m.AppendView("OBJ", loc.ObjTab{
+		UPCCode: "9902",
 	})
 
-	m.AppendView("OBJ", OBJ{
-		UPC: "8888",
+	m.AppendView("OBJ", loc.ObjTab{
+		UPCCode: "8888",
 	})
 	n := 1
-	m.AppendView("OBJ", OBJ{
-		UPC:     "0000000009087",
-		POINTER: &n,
+	m.AppendView("OBJ", loc.ObjTab{
+		UPCCode:      "0000000009087",
+		CategoryCode: &n,
 	})
-	m.AppendView("OBJ", OBJ{
-		UPC: "9999",
+	m.AppendView("OBJ", loc.ObjTab{
+		UPCCode: "9999",
 	})
 
 	// m["PRICE"] = Make("PRICE", loc.PRICE{})
